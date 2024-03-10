99d6aaab48c6a8c40f4aabd563393a1c070a0418
yangjian <yangj0412@foxmail.com>
2024-03-10 21:44:45 +0800

    feat(): register error

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser } = require("../service/user.service");
+const { createUser, getUserInfo } = require("../service/user.service");
 class UserController {
   // constructor({ userService }) {
   //   this.userService = userService;
@@ -11,6 +11,27 @@ class UserController {
     // console.log(ctx.request.body);
     // ctx.body = ctx.request.body;
     const { user_name, password } = ctx.request.body;
+
+    if (!user_name || !password) {
+      ctx.status = 400;
+      ctx.body = {
+        code: '10001',
+        message: 'user_name or password empty',
+        result: ''
+      }
+      return;
+    }
+
+    if (await getUserInfo({user_name})) {
+      ctx.status = 409;
+      ctx.body = {
+        code: '10002',
+        message: 'user already exist',
+        result: ''
+      }
+      return;
+    }
+
     const res = await createUser(user_name, password);
     console.log("res :>> ", res);
     ctx.body = {
